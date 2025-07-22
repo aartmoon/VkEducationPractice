003bb96c2be322fc3e11a4b0bdac12debdfa6e6d
artemsharkov <artemshar48@gmail.com>
2025-07-22 15:07:09 +0300

    f

---
+++ src/main/java/org/example/vkedupractice/VkEduPracticeApplication.java
@@ -0,0 +1,13 @@
+package org.example.vkedupractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VkEduPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VkEduPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/vkedupractice/VkEduPracticeApplicationTests.java
@@ -0,0 +1,12 @@
+package org.example.vkedupractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkEduPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
